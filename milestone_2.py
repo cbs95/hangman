56b45c98b1390b20e5c02256e0a87019d937eadc
cbs95 <147421641+cbs95@users.noreply.github.com>
2023-10-26 15:48:20 +0000

    Defined check_guess and ask_for_input functions and tested they both work.

--- milestone_2.py
+++ milestone_2.py
@@ -5,17 +5,26 @@ word_list = ["banana", "apple", "pear", "kiwi", "orange"]
 print(word_list)
 word = random.choice(word_list)
 print(word)
-play_hangman = True
-while play_hangman == True:
-    guess = input()
-    print(type(guess))
-    if guess.isalpha() and len(guess) == 1:
-        print("Good guess!")
-        play_hangman = False
+
+
+def check_guess(guess):
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Oops! That is not a valid input.")
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
-    
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    play_hangman = True
+    while play_hangman == True:
+        guess = input()
+        print(type(guess))
+        if guess.isalpha() and len(guess) == 1:
+            #print("Good guess!")
+            play_hangman = False
+            guess = guess.lower()
+            #return guess
+        else:
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
+
+ask_for_input()
