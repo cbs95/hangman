41ec955fe9e9941a7fb5bc3262e192490e25274c
cbs95 <147421641+cbs95@users.noreply.github.com>
2023-10-26 15:18:25 +0000

    Choose a random word from the list.

---
+++ milestone_2.py
@@ -0,0 +1,13 @@
+# @title Hangman Game
+import random
+
+word_list = ["banana", "apple", "pear", "kiwi", "orange"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+guess = input()
+print(type(guess))
+if guess.isalpha() and len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
