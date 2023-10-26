929eafad0aa5eb58056f933866686faf616c2522
cbs95 <3damltd@gmail.com>
2023-10-26 18:13:41 +0100

    Created new file with class and variables. Copied previous code from milestone_2.py.

---
+++ milestone_4.py
@@ -0,0 +1,38 @@
+# @title Hangman Game
+import random
+
+#class hangman():
+#    def __init__(self, word_list, num_lives = 5):
+
+word_list = ["banana", "apple", "pear", "kiwi", "orange"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+word_guessed = [*word]
+print(word_guessed)
+num_letters = len(list(dict.fromkeys(word_guessed)))
+print(num_letters)
+num_lives =
+list_of_guesses = []
+
+def check_guess(guess):
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    play_hangman = True
+    while play_hangman == True:
+        guess = input()
+        print(type(guess))
+        if guess.isalpha() and len(guess) == 1:
+            #print("Good guess!")
+            play_hangman = False
+            guess = guess.lower()
+            #return guess
+        else:
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
+
+ask_for_input()
